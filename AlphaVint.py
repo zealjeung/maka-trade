c34734d134df1c8a20ab4f356e9adb05a78832f3
Zealjeung Palmar <41066880+zealjeung@users.noreply.github.com>
2018-12-01 17:14:00 +0800

    Update AlphaVint.py

--- AlphaVint.py
+++ AlphaVint.py
@@ -6,29 +6,15 @@ import matplotlib.pyplot as plt  # module for plotting
 import os  # for input/output of files/directories
 import plotly.offline as pyo
 import plotly.graph_objs as go
-
-# to research the ticker symbol of us stocks
-dftick = pd.read_html(
-    'https://www.marketwatch.com/tools/markets/stocks/country/united-states/7')
-# change the last digit(page of url) to scan thru the ticker list
-# print(dftick[0])
-
-# Input database/ticker url:https://docs.quandl.com/docs/data-organization
-TICKER = 'EOD/AAPL'  # use either EOD or XNAS(datasets) / then ticker
-TICKER2 = TICKER[-4:]  # change according to number of ticker characters
-
-# Date Option 1 - use this only if current day is weekday(-1 converted to US)
-i = str(datetime.now())
-end_date = str(i[:10])
-# Date Option 2 - input desired end_date
-# end_date = '2018-08-17'
-
 from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
-os.chdir(r'C:\Users\jong\PycharmProjects\Stocks\US Stocks')
+
+TICKER = 'EOD/AMZN'  # use either EOD or XNAS(datasets) / then ticker
+TICKER2 = TICKER[-4:]  # change according to number of ticker characters
+symbol = TICKER2
+os.chdir(r'D:\Users\jong\PyProjects\Stocks\US Stocks')
 key = open('alphavantage.txt', 'r').read()
 ts = TimeSeries(key=key, output_format='pandas')
-data = data, meta_data = ts.get_daily_adjusted(symbol='AMZN',
+data = data, meta_data = ts.get_daily_adjusted(symbol=symbol,
                                                outputsize='full')
 data = data.reset_index()
 data['date'] = pd.to_datetime(data['date'])
@@ -52,18 +38,18 @@ Product_A = [go.Scatter(x=x,
                         ))]
 
 data1 = Product_A
-start = '2018-01-01'
-end = '2018-11-02'
+start = '2017-01-01'
+end = '2018-11-09'
 last = data.describe().loc['count', '4. close']
 
-layout = go.Layout(title='AAPL' + "  " + str(data['4. close'][-1]),
+layout = go.Layout(title=TICKER2 + "  " + str(data['4. close'][-1]),
                    titlefont=dict(size=28, color='rgb(51,204,153)'),
                    xaxis=dict(showgrid=True, color='lightgray',
                               range=[start, end]),
-                   yaxis=dict(title='$USD', showgrid=True, color='lightgray'),
+                   yaxis=dict(title='$USD', showgrid=True, color='gray'),
                    hovermode='x')
 
 fig = go.Figure(data=data1, layout=layout)
-path3 = (r'C:\Users\jong\PycharmProjects\Stocks\US Stocks')
+path3 = (r'D:\Users\jong\PyProjects\Stocks\US Stocks')
 os.chdir(path3)
 pyo.plot(fig, filename='USAlphaVint.html')
