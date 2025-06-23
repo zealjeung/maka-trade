42c9c6b2c2f0a8bdca5a9e2e7b461abb11935e2d
zealjeung <41066880+zealjeung@users.noreply.github.com>
2025-06-23 16:50:06 +0800

    Update Append.py

--- Append.py
+++ Append.py
@@ -1,46 +1,44 @@
-# setup panda for importing
-import pandas as pd
-from datetime import datetime
 import os
-path1 = (r'C:\Users\windows_10\PyProjects\Stocks\PSE screener\stockQuotes')
-path2 = (r'C:\Users\windows_10\PyProjects\Stocks\PSE screener')
-# os.chdir(path2)
-# to load latest csv file to a panda dataframe
-# input date in the ff. format MMDDYYYY
-prevdate = ('08152019')
-prevurl = '4044'
-end_date = ('08162019')
-endurl = '4045'
-#i = f'{datetime.now():%m%d%Y}'
-#end_date = str(i[:10])
-pseurl = str('https://www.pse.com.ph/stockMarket/marketInfo-marketActivity.html?tab=4')
-url = str('http://www.stockmarketpilipinas.com/attachment.php?aid='+endurl)
-c = pd.read_csv(url)
+import pandas as pd
 
-os.chdir(path1)
-c.to_csv('stockQuotes_' + end_date + '.csv')
+# --- CONFIGURATION ---
+# Set your data directory here or via environment variable
+DATA_DIR = os.environ.get("MAKA_TRADE_DATA_PATH", os.path.abspath(os.path.dirname(__file__)))
+PREV_DATE = '11282018'  # Update as needed
+END_DATE = '11292018'   # Update as needed
+NEW_CSV_FILENAME = 'stockQuotes_' + END_DATE + '.csv'
+CONSO_PREV_FILENAME = 'conso' + PREV_DATE + '.csv'
+CONSO_NEW_FILENAME = 'conso' + END_DATE + '.csv'
 
-os.chdir(path1)
-latestcsv = 'stockQuotes_' + end_date + '.csv'
-dfnew = pd.read_csv(url, names=['TICKER', 'DATE', 'OPEN',
-                    'HIGH', 'LOW', 'CLOSE', 'VOLUME', 'NFB'], index_col='DATE')
+# Path to the new stock quotes CSV
+new_csv_path = os.path.join(DATA_DIR, NEW_CSV_FILENAME)
+if not os.path.isfile(new_csv_path):
+    raise FileNotFoundError(f"Data file not found: {new_csv_path}")
 
+# Path to previous consolidated CSV
+conso_prev_path = os.path.join(DATA_DIR, CONSO_PREV_FILENAME)
+if not os.path.isfile(conso_prev_path):
+    raise FileNotFoundError(f"Data file not found: {conso_prev_path}")
 
-#os.chdir(path2)
-#latestcsv = 'stockQuotes_' + end_date + '.csv'
-#dfnew = pd.read_csv(latestcsv, names=['TICKER', 'DATE', 'OPEN',
-#                    'HIGH', 'LOW', 'CLOSE', 'VOLUME', 'NFB'], index_col='DATE')
+# Read new stock data
+dfnew = pd.read_csv(
+    new_csv_path,
+    names=['TICKER', 'DATE', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME', 'NFB'],
+    index_col='DATE'
+)
 
+# Read previous consolidated data
+dfconso = pd.read_csv(conso_prev_path, index_col='DATE')
 
-os.chdir(path2)
-# load current conso file
-dfconso = pd.read_csv('conso' + prevdate + '.csv', index_col='DATE')
+# Append new data and write to new consolidated file
+df_combined = pd.concat([dfconso, dfnew])
+conso_new_path = os.path.join(DATA_DIR, CONSO_NEW_FILENAME)
+df_combined.to_csv(conso_new_path)
 
-# to append latest csv to conso file and show stats before and after
-dfconso.append(dfnew).to_csv('conso' + end_date + '.csv')
-dfconsoprevdate = pd.read_csv('conso' + prevdate + '.csv')
-print(dfconsoprevdate.tail())
-dfconsoenddate = pd.read_csv('conso' + end_date + '.csv')
-print(dfconsoenddate.tail())
-print(prevdate, '+', end_date, '=', 'conso' + end_date + '.csv')
-print(len(dfconso), '+', len(dfnew), '=', len(dfconsoenddate))
+# Show before and after stats
+print("Previous consolidated CSV tail:")
+print(dfconso.tail())
+print("New stock quotes CSV tail:")
+print(dfnew.tail())
+print("New consolidated CSV tail:")
+print(df_combined.tail())
